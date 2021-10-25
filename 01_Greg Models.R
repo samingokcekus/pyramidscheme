b4cca6fbdd63bf9a56202055f89d5e808d860424
gfalbery <gfalbery@gmail.com>
2021-10-25 15:02:01 +0100

    Fixing models up

--- 01_Greg Models.R
+++ 01_Greg Models.R
@@ -29,9 +29,10 @@ DensityCovar <- c("LifetimeDensity", "AnnualDensity")
 #   ggpairs()
 
 ClashList <- list(
+  c("Strength_mean", "Degree"),
   c(SocialCovar[c(4:6)]),
   DensityCovar
-  )
+)
 
 Covar <- c("Age_num", "Year.w", "Largeoaks")
 
@@ -43,7 +44,7 @@ Resps %<>% sort
 
 ####FEMALE####
 
-DF <- DF_all[which(DF_all$Focal.sex == "F"),]
+# DF <- DF_all[which(DF_all$Focal.sex == "F"),]
 
 for(r in r:length(Resps)){
   
@@ -51,7 +52,9 @@ for(r in r:length(Resps)){
   
   if(0){
     
-    TestDF <- DF %>% dplyr::select(all_of(Covar), Focal.ring, Resps[r], X, Y) %>% na.omit
+    TestDF <- DF_all %>% 
+      filter(Focal.sex == "F") %>% 
+      dplyr::select(all_of(Covar), Focal.ring, Resps[r], X, Y) %>% na.omit
     
     if(Resps[r] == "April.lay.date"){
       
@@ -80,14 +83,18 @@ for(r in r:length(Resps)){
     
   }
   
-  TestDF <- DF %>% 
+  TestDF <- DF_all %>% 
+    filter(Focal.sex == "F") %>% 
     dplyr::select(all_of(Covar), all_of(SocialCovar), all_of(DensityCovar), 
                   Focal.ring, Resps[r], X, Y) %>% 
+    mutate_at("Year.w", as.factor) %>%
     mutate_at(SocialCovar, ~as.numeric(as.character(.x))) %>%
     mutate_at(SocialCovar[3], ~log(.x+1)) %>% mutate_at(SocialCovar[4], ~kader:::cuberoot(.x)) %>% 
     na.omit
   
-  TestDF %<>% mutate_at(c(Resps[r], SocialCovar), ~c(scale(.x)))
+  TestDF %>% nrow %>% print
+  
+  # TestDF %<>% mutate_at(c(Resps[r], SocialCovar), ~c(scale(.x)))
   
   IM2 <- INLAModelAdd(Data = TestDF, 
                       Response = Resps[r], 
@@ -102,8 +109,32 @@ for(r in r:length(Resps)){
   
   # IM2$FinalModel %>% Efxplot()
   
-  IMList[[Resps[r]]] <- IM2
+  IMList[[Resps[r]]] <- list()
   
+  IMList[[Resps[r]]]$Model1 <- IM2
+  
+  SocialKept <- SocialCovar %>% c(DensityCovar) %>% intersect(IM2$Kept)
+  
+  if(length(SocialKept) > 0){
+    
+    NewDF <- expand.grid(Var1 = SocialKept,
+                         Var2 = Covar) %>% 
+      mutate(Var = paste0(Var1, ":", Var2))
+    
+    IM3 <- INLAModelAdd(Data = TestDF, 
+                        Response = Resps[r], 
+                        Explanatory = IM2$Kept, 
+                        Add = NewDF$Var,
+                        AllModels = T,
+                        Base = T,
+                        # Rounds = 1,
+                        Clashes = ClashList,
+                        Random = "Focal.ring", RandomModel = "iid",
+                        AddSpatial = T)
+    
+    IMList[[Resps[r]]]$Model2 <- IM3
+    
+  } 
 }
 
 IMListF <- IMList
@@ -127,7 +158,7 @@ IMListF %>% names %>%
 
 ###MALE####
 
-DF <- DF_all[which(DF_all$Focal.sex == "M"),]
+# DF <- DF_all[which(DF_all$Focal.sex == "M"),]
 
 for(r in r:length(Resps)){
   
@@ -135,7 +166,9 @@ for(r in r:length(Resps)){
   
   if(0){
     
-    TestDF <- DF %>% dplyr::select(all_of(Covar), Focal.ring, Resps[r], X, Y) %>% na.omit
+    TestDF <- DF_all %>% 
+      filter(Focal.sex == "M") %>% 
+      dplyr::select(all_of(Covar), Focal.ring, Resps[r], X, Y) %>% na.omit
     
     if(Resps[r] == "April.lay.date"){
       
@@ -165,8 +198,10 @@ for(r in r:length(Resps)){
   }
   
   TestDF <- DF %>% 
+    filter(Focal.sex == "M") %>% 
     dplyr::select(all_of(Covar), all_of(SocialCovar), all_of(DensityCovar), 
                   Focal.ring, Resps[r], X, Y) %>% 
+    mutate_at("Year.w", as.factor) %>%
     mutate_at(SocialCovar, ~as.numeric(as.character(.x))) %>%
     mutate_at(SocialCovar[3], ~log(.x+1)) %>% mutate_at(SocialCovar[4], ~kader:::cuberoot(.x)) %>% 
     na.omit
@@ -186,8 +221,31 @@ for(r in r:length(Resps)){
   
   # IM2$FinalModel %>% Efxplot()
   
-  IMList[[Resps[r]]] <- IM2
+  IMList[[Resps[r]]] <- list()
+  IMList[[Resps[r]]]$Model1 <- IM2
   
+  SocialKept <- SocialCovar %>% c(DensityCovar) %>% intersect(IM2$Kept)
+  
+  if(length(SocialKept) > 0){
+    
+    NewDF <- expand.grid(Var1 = SocialKept,
+                         Var2 = Covar) %>% 
+      mutate(Var = paste0(Var1, ":", Var2))
+    
+    IM3 <- INLAModelAdd(Data = TestDF, 
+                        Response = Resps[r], 
+                        Explanatory = IM2$Kept, 
+                        Add = NewDF$Var,
+                        AllModels = T,
+                        Base = T,
+                        # Rounds = 1,
+                        Clashes = ClashList,
+                        Random = "Focal.ring", RandomModel = "iid",
+                        AddSpatial = T)
+    
+    IMList[[Resps[r]]]$Model2 <- IM3
+    
+  } 
 }
 
 IMListM <- IMList
